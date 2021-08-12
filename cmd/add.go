cb3ef42133de2e7c1d4f7ad972f28be3544918e0
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-11 21:41:15 -0700

    updated add function.

--- cmd/add.go
+++ cmd/add.go
@@ -2,16 +2,22 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+
+var strArgs []string
 
 var addCmd = cobra.Command{
 	Use: "add", 
 	Short: "Adds a task to your task list",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		strArgs = append(strArgs, args...)
+		todoItem := strings.Join(strArgs, " ")
+		fmt.Println(todoItem)
 	},
 }
 
