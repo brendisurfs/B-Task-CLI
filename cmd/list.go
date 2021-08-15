b7f8294b1d192f6d677807859a890ec40aa45463
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-15 12:36:13 -0700

    upaded list.go.

--- cmd/list.go
+++ cmd/list.go
@@ -2,20 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/brendisurfs/go-cli/db"
 	"github.com/spf13/cobra"
 )
 
-
 var listCmd = &cobra.Command{
-	Use: "list",	
+	Use:   "list",
 	Short: "list out your todos",
 
-	Run: func(cmd *cobra.Command, args []string ) {
-		fmt.Println("listing out your todos")
+	Run: func(cmd *cobra.Command, args []string) {
+		tasks, err := db.GetAllTasks()
+		if err != nil {
+			fmt.Println("something went wrong: ", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("you have no tasks to complete. Dont stress yourself out :)")
+			return
+		}
+		fmt.Println("you have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
-func init() { 
-	RootCmd.AddCommand(listCmd)	
+func init() {
+	RootCmd.AddCommand(listCmd)
 }
