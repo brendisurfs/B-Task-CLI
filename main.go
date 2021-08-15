36ba7a88f362c40b08cd58508b1b3a25ee282415
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-15 12:04:18 -0700

    finished adding bucket creation.

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/brendisurfs/go-cli/cmd"
+	"github.com/brendisurfs/go-cli/db"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func main() {
-	// runs our root command if no other args are passed into it.
-	cmd.RootCmd.Execute()
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	fmt.Println("db for tasks was created!")
+	// Execute app
+	must(cmd.RootCmd.Execute())
+}
+
+// small handler for our use case to catch errors.
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
